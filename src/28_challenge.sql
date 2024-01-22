1d3f2798c3b18cc49f1664b838623118f0cdc525
Thyago <chatack.tc@gmail.com>
2024-01-22 14:47:30 -0300

    [feat] requisito 28 feito

---
+++ src/28_challenge.sql
@@ -0,0 +1,8 @@
+SELECT
+  users.full_name AS 'Pessoa usuária',
+  COUNT(artists_followers.user_id) 'Artistas que segue'
+FROM users
+  INNER JOIN artists_followers
+    ON users.id = artists_followers.user_id
+GROUP BY users.full_name
+ORDER BY users.full_name;
