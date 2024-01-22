97b30c730c0225dc90a84f7efa9f2b3b249f81b5
Thyago <chatack.tc@gmail.com>
2024-01-22 08:52:15 -0300

    [feat] requisito 19 feito

---
+++ src/19_challenge.sql
@@ -0,0 +1,9 @@
+SELECT
+  songs.title 'Título',
+  duration_in_seconds 'Duração',
+  albums.title 'Álbum'
+FROM songs
+  INNER JOIN albums
+    ON songs.album_id = albums.id
+WHERE duration_in_seconds BETWEEN 300 AND 480
+ORDER BY duration_in_seconds ASC;
