20ed82578cfcd66f3584c4c3d6bf99f3c8eea825
Thyago <chatack.tc@gmail.com>
2024-01-22 10:13:54 -0300

    [feat] requisito 22 feito

---
+++ src/22_challenge.sql
@@ -0,0 +1,11 @@
+SELECT 
+  artists.name 'Artista',
+  COUNT(songs.title) 'Quantidade de músicas'
+FROM artists
+  INNER JOIN albums
+    ON artists.id = albums.artist_id
+  INNER JOIN songs
+    ON albums.id = songs.album_id
+GROUP BY artists.name
+ORDER BY COUNT(songs.title) DESC, artists.name
+LIMIT 3;
